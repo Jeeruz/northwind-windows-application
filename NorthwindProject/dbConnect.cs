cf57e854585bbf4ba8d34a32eb59a8c15317626d
Jeeruz <jdelacruz.proacc@gmail.com>
2024-04-07 02:14:07 +0800

    updated connection to sql again

--- NorthwindProject/dbConnect.cs
+++ NorthwindProject/dbConnect.cs
@@ -12,19 +12,20 @@ namespace NorthwindProject
         // declare sql parameter for later initialization 
         public static SqlConnection dbConnectInst = null;
 
-        // use this to connect to a company's network database
+        // use this if you want to use sql server authentication
         // create connection string that has details of the local database 
-        //public static string dbConnectionString = @"Data Source=10.10.1.01;"    // sql servername 
-        //                                          +"Initial Catalog=NORTHWIND;" // sql database name 
-        //                                          +"Integrated Security=false;" // ssl security
-        //                                          +"User ID = admin;"           // sql username 
-        //                                          +"Password = Default2024;";   // sql password 
+        public static string dbConnectionString = @"Data Source=LAPTOP-JEFF;"    // sql servername 
+                                                  + "Initial Catalog=NORTHWIND;" // sql database name 
+                                                  + "Integrated Security=false;" // ssl security
+                                                  + "User ID = jeff;"           // sql username 
+                                                  + "Password = 123;";   // sql password 
 
+        // use this if you want to use windows authentication
         // create connection string that has details of the local database 
-        public static string dbConnectionString = @"Server=LAPTOP-JEFF;"          // sql servername 
-                                                + "Database=NORTHWIND;"           // sql database name 
-                                                + "Integrated Security=True;";    // ssl security 
-         
+        //public static string dbConnectionString = @"Server=LAPTOP-JEFF;"          // sql servername 
+        //                                        + "Database=NORTHWIND;"           // sql database name 
+        //                                        + "Integrated Security=True;";    // ssl security 
+
         // used to open connection to database 
         public static void dbConnectOpen()
         {
